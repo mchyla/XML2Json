71260bea6341d68f250335c18fd19df04ba1854a
mchyla <marcinchyla92@gmail.com>
2017-02-02 00:14:59 +0100

    new gitignore rules, added Main.java

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+   
+public class Main 
+{
+  static public void main(String argv[]) 
+  {      
+    try 
+    {
+      parser p = new parser(new MyLexer(new FileReader(argv[0])));
+      Object result = p.parse().value;      
+    } 
+    catch (Exception e) 
+    {     
+      e.printStackTrace();
+    }
+  }
+}
+
+
